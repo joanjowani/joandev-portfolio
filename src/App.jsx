df7a1dc34c587eb61342b564c9fa7c6111b9d84c
Joan Jowani <joan.kirstienborja17@gmail.com>
2025-08-05 02:04:57 +0800

    Fix ui issues

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,22 @@
 
 // export default App
 
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
+ 
   return (
     <>
       <Toaster />
-      <Home />
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
